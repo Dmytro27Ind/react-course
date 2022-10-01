6ba46a04f0a1a1e1fe5f324a4cc7946ab3665099
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-10-01 15:39:21 +0200

    useState

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// eslint-disable-next-line
+let renderFirst = [
+  <div>
+    App is working !
+    <button>Button 1</button>
+    {React.createElement('button', {
+      onClick: () => console.log('Click')
+    }, 'Button 2')}
+  </div>,
+
+  React.createElement('button', {
+    onClick: () => console.log('Click')
+  }, 'Button 3')
+]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // renderFirst
   <App />
 );
