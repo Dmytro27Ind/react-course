bc0977186af8af300ca7dfb6ffe272c3fc97b216
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-10-03 00:39:53 +0200

    porps in components and styles

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import './styles/App.css'
 
 function App() {
-  const [likes, setLikes] = useState(0)
-  const [value, setValue] = useState('Input text')
 
-  function increment(){
-    setLikes(likes + 1)
-  }
+  //* first app
+  // const [likes, setLikes] = useState(0)
+  // const [value, setValue] = useState('Input text')
 
-  function decrement(){
-    setLikes(likes - 1)
-  }
+  // function increment(){
+  //   setLikes(likes + 1)
+  // }
+  // function decrement(){
+  //   setLikes(likes - 1)
+  // }
+  // // eslint-disable-next-line
+  // const appOne = <div class="App">
+  //   <h1>{likes}</h1>
+  //   <h1>{value}</h1>
+  //   <input type="text" value={value} onChange={
+  //     event => setValue(event.target.value)
+  //   }/>
+  //   <button onClick={increment}>Increment</button>
+  //   <button onClick={decrement}>Decrement</button>
+
+  //   <Counter/>
+  //   <Counter/>
+  //   <Counter/>
+
+  //   <ClassCounter/>
+  //   <ClassCounter/>
+  //   <ClassCounter/>
+  // </div>;
+
+  
 
   return (
-    <div class="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input type="text" value={value} onChange={
-        event => setValue(event.target.value)
-      }/>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-
-      <Counter/>
-      <Counter/>
-      <Counter/>
-
-      <ClassCounter/>
-      <ClassCounter/>
-      <ClassCounter/>
+    // appOne;
+    <div className="App">
+      <PostItem post={{id: 1, title: 'JavaScript', body: 'Javascript - programing language'}}/>
+      <PostItem post={{id: 2, title: 'JavaScript', body: 'Javascript - programing language'}}/>
+      <PostItem post={{id: 3, title: 'JavaScript', body: 'Javascript - programing language'}}/>
     </div>
   );
 }
