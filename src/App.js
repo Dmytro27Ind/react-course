6cac7009334d3f93bc25405074d852d44cefb00c
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-10-03 01:24:57 +0200

    PostList component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
-import PostItem from "./components/PostItem";
+// import ClassCounter from "./components/ClassCounter";
+// import Counter from "./components/Counter";
+import PostList from "./components/PostList";
 import './styles/App.css'
 
 function App() {
@@ -35,14 +35,23 @@ function App() {
   //   <ClassCounter/>
   // </div>;
 
-  
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'},
+  ])
+
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Python 2', body: 'Description'},
+    {id: 3, title: 'Python 3', body: 'Description'},
+  ])
 
   return (
     // appOne;
     <div className="App">
-      <PostItem post={{id: 1, title: 'JavaScript', body: 'Javascript - programing language'}}/>
-      <PostItem post={{id: 2, title: 'JavaScript', body: 'Javascript - programing language'}}/>
-      <PostItem post={{id: 3, title: 'JavaScript', body: 'Javascript - programing language'}}/>
+      <PostList posts={posts} title="List of posts about JS"/>
+      <PostList posts={posts2} title="List of posts about Python"/>
     </div>
   );
 }
