d8cb5944640d712f02c5f77e58f7648133cffb9e
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-11-30 00:23:26 +0100

    custom use hook

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import PostForm from "./components/PostForm";
 // import Counter from "./components/Counter";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import MyModal from "./components/UI/MyModal/MyModal";
-import MySelect from "./components/UI/select/MySelect";
+import { usePosts } from "./hooks/usePosts";
 import './styles/App.css'
 
 function App() {
@@ -48,18 +47,7 @@ function App() {
   ])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    console.log('call getSortedPosts()')
-    if(filter.sort){
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
