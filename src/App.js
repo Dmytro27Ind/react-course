3463021f97b8ac49a4b5e51210149467c8a7f20e
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-01-02 00:52:18 +0100

    Decomposing of router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
-import About from "./components/UI/pages/About";
-import Posts from "./components/UI/pages/Posts";
-import Error from "./components/UI/pages/Error";
 import './styles/App.css'
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
-      <Routes>
-        <Route exact path="/about" element={<About/>}/>
-        <Route exact path="/posts" element={<Posts/>}/>
-        <Route exact path="/error" element={<Error/>}/>
-        {/* 👇️ only match this when no other routes match */}
-        <Route path="*" element={<Navigate to="/error"/>} />
-      </Routes>
+      <AppRouter/>
     </BrowserRouter>
   )
 }
---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+import About from "../components/UI/pages/About";
+import Posts from "../components/UI/pages/Posts";
+import Error from "../components/UI/pages/Error";
+
+function AppRouter() {
+  return (
+    <Routes>
+        <Route exact path="/about" element={<About/>}/>
+        <Route exact path="/posts" element={<Posts/>}/>
+        <Route exact path="/error" element={<Error/>}/>
+        {/* 👇️ only match this when no other routes match */}
+        <Route path="*" element={<Navigate to="/error"/>} />
+    </Routes>
+  )
+}
+
+export default AppRouter
