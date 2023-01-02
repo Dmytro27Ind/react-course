79623de6a60cd4571facd835ca0a1fabc86270f5
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-01-02 23:11:23 +0100

    Comments for post

--- src/components/UI/pages/PostIdPage.jsx
+++ src/components/UI/pages/PostIdPage.jsx
@@ -6,14 +6,20 @@ import Loader from '../Loader/Loader'
 
 function PostIdPage() {
   const params = useParams()
-  const [post, setPost] = useState(null)
+  const [post, setPost] = useState({})
+  const [comments, setComments] = useState([])
   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
     const response = await PostService.getById(id)
     setPost(response.data)
   })
+  const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+    const response = await PostService.getCommentsByPostId(id)
+    setComments(response.data)
+  })
 
   useEffect(() => {
     fetchPostById(params.id)
+    fetchComments(params.id)
   }, [])
 
   return (
@@ -22,6 +28,18 @@ function PostIdPage() {
         {isLoading
           ? <Loader/>
           : <div>{post?.id}. {post?.title}</div>
+        }
+        <h1>Comments:</h1>
+        {isComLoading
+          ? <Loader/>
+          : <div>
+            {comments.map(comm =>
+              <div style={{marginTop: 15}}>
+                <h5>{comm.email}</h5>
+                <div>{comm.body}</div>
+              </div>
+            )}
+          </div>
         }
     </div>
   )
